43e68eb349b719211a908b279050fd94712d68ec
Sean Hellum <seanhellum45@gmail.com>
2019-11-04 22:26:48 +0000

    yep

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if &args[1] == "add" {
-        println!(
-            "The sum is: {}",
-            &args[2].parse::<f64>().unwrap() + &args[3].parse::<f64>().unwrap()
-        );
+        let slice = &args[2..args.len()];
+        let mut sum: f64 = 0.0;
+        for i in slice {
+            sum = sum + i.parse::<f64>().unwrap();
+        }
+        println!("The sum is: {}", sum);
     } else if &args[1] == "subtract" {
-        println!(
-            "The diffrence is: {}",
-            &args[2].parse::<f64>().unwrap() - &args[3].parse::<f64>().unwrap()
-        );
+        let slice = &args[3..args.len()];
+        let mut diffrence: f64 = args[2].parse::<f64>().unwrap();
+        for i in slice {
+            diffrence = diffrence - i.parse::<f64>().unwrap();
+        }
+        println!("The sum is: {}", diffrence);
     }
 }
