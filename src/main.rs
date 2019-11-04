0dc8b5fb9071b842f4c0662d643b6e354378270e
Sean Hellum <seanhellum45@gmail.com>
2019-11-04 20:34:41 +0000

    addition function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if &args[1] == "add" {
+        println!("The sum is: {}", &args[2].parse::<f64>().unwrap() + &args[3].parse::<f64>().unwrap());
+    }
 }
