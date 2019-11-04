47c66ce3a0f72c7364e2f102a236b7dc78ed6bb0
Sean Hellum <seanhellum45@gmail.com>
2019-11-04 20:37:01 +0000

    format

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ use std::env;
 fn main() {
     let args: Vec<_> = env::args().collect();
     if &args[1] == "add" {
-        println!("The sum is: {}", &args[2].parse::<f64>().unwrap() + &args[3].parse::<f64>().unwrap());
+        println!(
+            "The sum is: {}",
+            &args[2].parse::<f64>().unwrap() + &args[3].parse::<f64>().unwrap()
+        );
     } else if &args[1] == "subtract" {
-        println!("The diffrence is: {}", &args[2].parse::<f64>().unwrap() - &args[3].parse::<f64>().unwrap());
+        println!(
+            "The diffrence is: {}",
+            &args[2].parse::<f64>().unwrap() - &args[3].parse::<f64>().unwrap()
+        );
     }
 }
